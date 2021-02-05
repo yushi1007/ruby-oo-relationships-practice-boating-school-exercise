708af2d0dd21379a40bc6cf29f2d6fed57a01a49
Yu Shi <yushiys95@gmail.com>
2021-02-05 16:55:52 -0500

    commit tree 403d7797dc91fdb777c437eb49e9f557ca5d776c

--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,6 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+#binding.pry
+#0 #leave this here to ensure binding.pry isn't the last line
 
